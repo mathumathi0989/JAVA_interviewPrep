b0ac35b763477706b2ac98ed4042212e35b1e977
mathumathi0989 <mathumathi.b@gmail.com>
2025-04-15 12:43:54 -0400

    few coding

--- src/interviewPrepJava/javaCodingProb.java
+++ src/interviewPrepJava/javaCodingProb.java
@@ -1,5 +1,9 @@
 package interviewPrepJava;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class javaCodingProb {
 
 	
@@ -26,13 +30,40 @@ public class javaCodingProb {
 		}
 		
 		return word.equalsIgnoreCase(temp);
-//		if (word.equals(temp)) {
-//			return true;
-//		}
+		}
+		return false;
+	}
+	
+	
+	//find number of words in a given string 
+	int countWords(String sentence) {
+		
+		String[] word = sentence.trim().split(" ");
+	
+		int count = 0;
+		for(int i =0; i<word.length; i++) {
+			System.out.println(word[i]);
+			count++;
+			
 		}
 		
+		return count;	
+	}
+	
+	//find number of each character in a given string
+	void charCount(String word) {
 		
-		return false;
+		Map<Character, Integer> map = new HashMap<>();
+		
+		for(int i=0; i<word.length(); i++) {
+			char ch = word.charAt(i);
+			map.put(ch, map.getOrDefault(ch, 0)+1);
+		}
+		
+		for(Entry<Character, Integer> entry: map.entrySet()) {
+			System.out.println(entry.getKey() +":" + entry.getValue());
+		}
+	
 	}
 	
 	
@@ -40,10 +71,11 @@ public class javaCodingProb {
 	//	System.out.println("Hello world");
 		javaCodingProb code = new javaCodingProb();
 	//	code.reverse("letcode");
-		System.out.println(code.palindrome("madam"));
-		System.out.println(code.palindrome("nurses run"));
-		System.out.println(code.palindrome("mathu"));
-		
+//		System.out.println(code.palindrome("madam"));
+//		System.out.println(code.palindrome("nurses run"));
+//		System.out.println(code.palindrome("mathu"));
+	//	System.out.println(code.countWords("hey, happy to see you here"));
+		code.charCount("letcode");
 		
 	}
 }
